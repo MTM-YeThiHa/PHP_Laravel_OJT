1eab9b7a00580ad6ec7bece589d1402ede9d3405
Ye Thiha <scm.yethiha@gmail.com>
2023-11-07 13:51:56 +0630

    modified change password autocomplete value

--- laravel_php_app/resources/views/users/change-password.blade.php
+++ laravel_php_app/resources/views/users/change-password.blade.php
@@ -31,7 +31,7 @@
               <label for="password" class="col-md-4 col-form-label text-md-right">New Password</label>
 
               <div class="col-md-6">
-                <input id="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password" value="{{ old('new_password') }}" autocomplete="current-password">
+                <input id="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password" value="{{ old('new_password') }}">
                 @error('new_password')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
@@ -44,7 +44,7 @@
               <label for="password" class="col-md-4 col-form-label text-md-right">New Confirm Password</label>
 
               <div class="col-md-6">
-                <input id="new_confirm_password" type="password" class="form-control @error('new_confirm_password') is-invalid @enderror" name="new_confirm_password" value="{{ old('new_confirm_password') }}" autocomplete="current-password">
+                <input id="new_confirm_password" type="password" class="form-control @error('new_confirm_password') is-invalid @enderror" name="new_confirm_password" value="{{ old('new_confirm_password') }}">
                 @error('new_confirm_password')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
